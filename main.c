6c0cf42f0d1a8e7e0e81b0a3cd899d0181ea02ca
Rakshita-Shetty <73981158+Rakshita-Shetty@users.noreply.github.com>
2020-12-22 10:40:24 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+#include " stock.h"
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+q = queue_add(q,10, &res);
+q = queue_add(q,20, &res);
+assert(queue_length(q) == 3);
+assert(res.status == queue_full);
+
+
+
+void test_Qdelete(){
+Queue q1 = queue_new(3);
+Queue +q =  &q1;
 }
